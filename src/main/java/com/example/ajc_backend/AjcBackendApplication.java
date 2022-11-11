967d6ebf4ec17e25554834a9a2519749235a39a6
TCHIENDJE Lucien <tchiendjelucien@gmail.com>
2022-11-11 13:09:01 +0100

    Mon premier commit

---
+++ src/main/java/com/example/ajc_backend/AjcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ajc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjcBackendApplication.class, args);
+	}
+
+}
